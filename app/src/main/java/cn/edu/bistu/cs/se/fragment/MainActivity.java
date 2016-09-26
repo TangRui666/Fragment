32c4efadb0c39b745dd473e0e076c5d8eb37bf38
TangRui666 <654427660@qq.com>
2016-09-26 13:41:16 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/fragment/MainActivity.java
@@ -0,0 +1,63 @@
+package cn.edu.bistu.cs.se.fragment;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import cn.edu.bistu.cs.se.fragment.Words.WordsContent;
+
+public class MainActivity extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener,DetailFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onListFragmentInteraction(WordsContent.WordItem item) {
+        Bundle arguments =new Bundle();
+        arguments.putString("id",item.id);
+        DetailFragment fragment=new DetailFragment();
+        fragment.setArguments(arguments);
+        getFragmentManager().beginTransaction().replace(R.id.worddetail,fragment).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+
+
+    }
+}
+
